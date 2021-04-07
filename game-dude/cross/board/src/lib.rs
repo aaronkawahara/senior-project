7b375a48a425c9762f126a9ee339326b9a245c9f
theayayron <aaronkawahara@gmail.com>
2021-04-07 16:44:58 -0700

    added initial board lib.rs template

---
+++ game-dude/cross/board/src/lib.rs
@@ -0,0 +1,12 @@
+// Board/peripheral initialization
+// note: create new folder for each peripheral/system ex) lcd, buttons, etc.
+
+pub struct Board {
+    // peripherals
+}
+
+impl Board {
+    pub fn init(/*peripherals*/) {
+
+    }
+}
