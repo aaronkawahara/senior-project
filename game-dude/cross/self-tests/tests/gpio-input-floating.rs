f388efa472c1024a0edd8f7feab3cc74c18ba2e8
theayayron <aaronkawahara@gmail.com>
2021-04-09 14:13:35 -0700

    self-tests changes to match ferrous systems guide

--- game-dude/cross/self-tests/tests/gpio-input-floating.rs
+++ game-dude/cross/self-tests/tests/gpio-input-floating.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use defmt_rtt as _;    // transport layer for defmt logs
-//use stm32l4xx_hal as _; // the HAL we'll test
+use stm32l4xx_hal as _; // the HAL we'll test
 use panic_probe as _;  // panicking behavior
 
 #[defmt_test::tests]
