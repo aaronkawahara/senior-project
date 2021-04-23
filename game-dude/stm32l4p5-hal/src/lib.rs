797799150354291216bbcbaf21fdb1a239fa986a
theayayron <aaronkawahara@gmail.com>
2021-04-23 14:34:54 -0700

    reconfigured import using new pac

--- game-dude/stm32l4p5-hal/src/lib.rs
+++ game-dude/stm32l4p5-hal/src/lib.rs
@@ -3,9 +3,9 @@
 pub use embedded_hal as hal;
 pub use nb;
 pub use nb::block;
-pub use stm32l4::stm32l4p5 as pac;
+pub use stm32l4p5_pac as pac;
 pub use pac as stm32;
-pub use crate::pac::interrupt;
+pub use crate::pac::Interrupt;
 
 // peripheral exports
 pub mod gpio;
