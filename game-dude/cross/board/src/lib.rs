356b95bf62375f8ca9aab16765bb3a1ab0bc320b
theayayron <aaronkawahara@gmail.com>
2021-04-24 17:41:50 -0700

    removed old commented out code

--- game-dude/cross/board/src/lib.rs
+++ game-dude/cross/board/src/lib.rs
@@ -52,53 +52,21 @@ impl Board {
         rcc.pllsai2cfgr
             .sai2m_div(2)
             .sai2n_mult(12)
-            .lcd_div(rcc::pllsai2cfgr::PLLSAI2R_A::DIV8);
+            .lcd_div(rcc::pllsai2cfgr::PLLSAI2R_A::DIV8)
+            .freeze();
         
-        // Board::init_lcd(&mut rcc);
+        // GPIO Configuration
 
+
+        // LCD Configuration
+        
         Self {
             lcd: Lcd
         }
     }
 
-    /*fn init_rcc(rcc: &mut rcc::Rcc, flash: &mut flash::Parts, pwr: &mut Pwr) {
-        let input_divider = 1;
-        let multiplier = 10;
-        let pll_config = rcc::PllConfig::new(
-            input_divider,
-            multiplier,
-             rcc::PllDivider::Div2
-        );
-
-        let clocks = rcc
-            .cfgr
-            .msi(rcc::MsiFreq::RANGE48M)
-            .pll_source(rcc::PllSource::HSI16)
-            .sysclk_with_pll(MAX_SYSCLK, pll_config)
-            .pclk1(MAX_SYSCLK)
-            .pclk2(MAX_SYSCLK)
-            .freeze(&mut flash.acr, &mut pwr);
-
-        // let pllsai1 = stm32::rcc::pllsai1cfgr();
-    }
-
-    fn init_pwr(pwr: &mut Pwr) {
-        // set PWR_CR5 to boost mode
-    }
-
+    /*
     fn init_lcd(rcc: &mut rcc::Rcc) {
-        // enable LTDC clk in RCC_CR
-        // The LTDC clock. The LTDC clock is generated from a specific PLL (PLLSAI2) RCC_PLLCFGR
-
-        // config pixel clk
-        // config synchronous timings for HSYNC + VSYNC
-        // config synchronous signals and clk polarity in LTDC_GCR reg
-        // config background color
-        // config interrupts in LTDC_IER & LTDC_LIPCR
-        // config l1 params
-        // enable l1 and CLUT in LTDC_LxCR reg
-        // dithering and color keying optional
-        // reload shadow registers to active register through LTDC_SRCR
-        // enable LCD-TFT controller in LTDC_GCR
+        
     } */
 }
