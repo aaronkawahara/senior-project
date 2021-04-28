bb844baa6ef590bd4ce07a984f3a1f1b00bd5346
theayayron <aaronkawahara@gmail.com>
2021-04-28 15:43:31 -0700

    new test

--- game-dude/cross/app/src/main.rs
+++ game-dude/cross/app/src/main.rs
@@ -1,6 +1,22 @@
 // decide on rtic or not.. will greatly impact flow
 #![no_std]
+#![no_main]
 
-fn main() {
+use cortex_m_rt as rt;
 
+use rt::entry;
+use defmt_rtt as _;
+use panic_probe as _;
+use board::{self, Board};
+
+#[entry]
+fn main() -> ! {
+    let mut board = Board::init();
+
+    loop {
+        board.lcd.set_color(0xFF);
+        board.lcd.reload_srcr();
+        while board.lcd.vsync_is_high() {}
+        while !board.lcd.vsync_is_high() {}
+    }
 }
